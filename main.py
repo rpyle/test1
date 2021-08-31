f859b8fadc235c9a09fee650f415a31d8e3e4aa6
Robert Pyle <rpyle@guhsd.net>
2021-08-31 20:06:47 +0000

    uncommented code

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ spider = trtl.Turtle()
 spider.pensize(40)
 spider.circle(20)
 
-'''
 w = 6
 y = 70
 z = 380 / w
-x.pensize(5)
+spider.pensize(5)
 n = 0
 while (n < w):
-  x.goto(0,0)
-  x.setheading(z*n)
-  x.forward(y)
-  n = n + 1
-x.hideturtle()
-'''
+    spider.goto(0, 0)
+    spider.setheading(z * n)
+    spider.forward(y)
+    n = n + 1
+spider.hideturtle()
 
 wn = trtl.Screen()
 wn.mainloop()
